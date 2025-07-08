66ff04c20e29e38883c0b273dc060411714a09ba
defaultkavy <defaultkavy@gmail.com>
2025-07-08 23:39:45 +0800

    complete `$Anchor` methods interface.

--- ext/html/node/$Anchor.ts
+++ ext/html/node/$Anchor.ts
@@ -8,8 +8,36 @@ export class $Anchor extends $HTMLElement<HTMLAnchorElement> {
 }
 
 export interface $Anchor extends $HTMLElement<HTMLAnchorElement> {
-    href(href: string): this;
+    /** {@link HTMLAnchorElement.href} */
+    href(href: $Parameter<string>): this;
     href(): string;
+    /** {@link HTMLAnchorElement.hreflang} */
+    hreflang(hreflang: $Parameter<string>): this;
+    hreflang(): string;
+    /** {@link HTMLAnchorElement.download} */
+    download(download: $Parameter<string>): this;
+    download(): string;
+    /** {@link HTMLAnchorElement.ping} */
+    ping(ping: $Parameter<string>): this;
+    ping(): string;
+    /** {@link HTMLAnchorElement.referrerPolicy} */
+    referrerPolicy(referrerPolicy: $Parameter<string>): this;
+    referrerPolicy(): string;
+    /** {@link HTMLAnchorElement.rel} */
+    rel(rel: $Parameter<string>): this;
+    rel(): string;
+    /** {@link HTMLAnchorElement.relList} */
+    relList(relList: $Parameter<string>): this;
+    relList(): DOMTokenList;
+    /** {@link HTMLAnchorElement.target} */
+    target(target: $Parameter<string>): this;
+    target(): string;
+    /** {@link HTMLAnchorElement.text} */
+    text(text: $Parameter<string>): this;
+    text(): string;
+    /** {@link HTMLAnchorElement.type} */
+    type(type: $Parameter<string>): this;
+    type(): string;
 }
 
 declare module '#core' {

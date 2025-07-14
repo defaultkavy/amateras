443d100bdead961e8c5e753d28bb8e538f04f5e9
defaultkavy <defaultkavy@gmail.com>
2025-07-14 17:59:50 +0800

    export `$HTMLElement` from `amateras/node`

--- src/node/node.ts
+++ src/node/node.ts
@@ -3,8 +3,9 @@ import { assignHelper } from '#lib/assignHelper';
 import { $Element } from '#node/$Element';
 import { $Node, $Text } from './$Node';
 
-export * from './$Element';
 export * from './$Node';
+export * from './$Element';
+export * from './$HTMLElement';
 
 assignHelper(Node, $Node);
 assignHelper(Text, $Text);

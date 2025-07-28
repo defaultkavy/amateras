7c2f8f71c6cc3e74c019efac3893ee7745ee3427
defaultkavy <defaultkavy@gmail.com>
2025-07-28 09:18:56 +0800

    - added `equal` function to `lib/native.ts`. - optimize code size by using arrow function in `lib/native.ts`.

--- src/lib/native.ts
+++ src/lib/native.ts
@@ -9,42 +9,26 @@ export const _Object_defineProperty = Object.defineProperty;
 export const _Object_getOwnPropertyDescriptors = Object.getOwnPropertyDescriptors;
 // Array
 export const _Array_from = Array.from;
-export function forEach<N extends Node>(set: NodeListOf<N>, fn: (value: N, index: number, parent: NodeListOf<N>) => any, thisArgs?: any): void;
-export function forEach<K, V>(set: Map<K, V>, fn: (value: V, key: K, index: number, map: Map<K, V>) => any, thisArgs?: any): void;
-export function forEach<T>(set: Set<T>, fn: (value: T, index: number, set: Set<T>) => any, thisArgs?: any): void;
-export function forEach<T>(arr: Array<T>, fn: (value: T, index: number, array: Array<T>) => any, thisArgs?: any): void;
-export function forEach<T>(arr: any, fn: any, thisArgs?: any) {
-    arr.forEach(fn, thisArgs)
-}
-// typeof
-export function _typeof(target: any, type: 'string' | 'number' | 'object' | 'boolean' | 'function' | 'bigint' | 'symbol' | 'undefined') {
-    return typeof target === type;
-}
-export function isString(target: any): target is string {
-    return _typeof(target, 'string');
-}
-export function isNumber(target: any): target is number {
-    return _typeof(target, 'number')
-}
-export function isObject(target: any): target is object {
-    return _typeof(target, 'object')
-}
-export function isFunction(target: any): target is Function {
-    return _typeof(target, 'function')
-}
-export function isUndefined(target: any): target is undefined {
-    return _typeof(target, 'undefined')
-}
-export function isNull(target: any): target is null {
-    return target === null;
-}
-export function _instanceof<T extends (abstract new (...args: any[]) => any)[]>(target: any, ...instance: T): target is InstanceType<T[number]> {
-    return !!instance.find(i => target instanceof i);
-}
+export const forEach: forEach = <T>(arr: any, fn: any, thisArgs?: any) => arr.forEach(fn, thisArgs);
+// type check
+export const _typeof = (target: any, type: 'string' | 'number' | 'object' | 'boolean' | 'function' | 'bigint' | 'symbol' | 'undefined') => typeof target === type;
+export const equal = <T, V extends T>(target: T, ...args: V[]): target is V => !!args.find(a => a === target);
+export const isString = (target: any): target is string => _typeof(target, 'string');
+export const isNumber = (target: any): target is number => _typeof(target, 'number')
+export const isObject = (target: any): target is object => _typeof(target, 'object')
+export const isFunction = (target: any): target is Function => _typeof(target, 'function')
+export const isUndefined = (target: any): target is undefined => _typeof(target, 'undefined')
+export const isNull = (target: any): target is null => target === null;
+export const _instanceof = <T extends (abstract new (...args: any[]) => any)[]>(target: any, ...instance: T): target is InstanceType<T[number]> => !!instance.find(i => target instanceof i);
 // JSON
 export const _JSON_stringify = JSON.stringify;
 export const _JSON_parse = JSON.parse;
 // String
-export function startsWith(target: string, ...str: string[]) {
-    return !!str.find(s => target.startsWith(s))
+export const startsWith = (target: string, ...str: string[]) => !!str.find(s => target.startsWith(s));
+
+interface forEach {
+    <T>(arr: Array<T>, fn: (value: T, index: number, array: Array<T>) => any, thisArgs?: any): void;
+    <T>(set: Set<T>, fn: (value: T, index: number, set: Set<T>) => any, thisArgs?: any): void;
+    <K, V>(set: Map<K, V>, fn: (value: V, key: K, index: number, map: Map<K, V>) => any, thisArgs?: any): void;
+    <N extends Node>(set: NodeListOf<N>, fn: (value: N, index: number, parent: NodeListOf<N>) => any, thisArgs?: any): void;
 }

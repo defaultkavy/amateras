da781e546c641fc46213ca929b308dd34a14a069
defaultkavy <defaultkavy@gmail.com>
2025-10-16 16:46:00 +0800

    [css/variables] new `set` and `reset` methods allows override value to variables.

--- ext/css/src/structure/$CSSVariable.ts
+++ ext/css/src/structure/$CSSVariable.ts
@@ -1,9 +1,23 @@
+import { _document } from "../../../../src/lib/env";
+
+const documentElementStyle = _document.documentElement.style;
+
 export class $CSSVariable<V = string> {
     key: string;
     value: V;
     constructor(key: string, value: V) {
         this.key = key;
         this.value = value;
+    }
+
+    set(value: string) {
+        documentElementStyle.setProperty(`--${this.key}`, value);
+        return this;
+    }
+
+    reset() {
+        documentElementStyle.removeProperty(`--${this.key}`);
+        return this;
     }
 
     toString() {

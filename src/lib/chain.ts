50649ecce4c47a632b253e4ffed160a30edcffc2
defaultkavy <defaultkavy@gmail.com>
2025-10-16 19:29:24 +0800

    [core] add extensible setter on `chain` function

--- src/lib/chain.ts
+++ src/lib/chain.ts
@@ -1,11 +1,15 @@
+import { $Node } from "#node/$Node";
 import { isUndefined } from "./native";
 
-export const chain: chain = <T, R, V>(_this: T, args: IArguments | null, get: (() => R) | null, value: V, set: (value: Exclude<V, undefined>) => any) => 
-    args && get && !args.length 
-    ? get()
-    : isUndefined(value) 
-        ? _this 
-        : (set(value as any), _this);
+export const chain: chain = <T, R, V>(_this: T, args: IArguments | null, get: (() => R) | null, value: V, set: (value: Exclude<V, undefined>) => any) => {
+    if (args && get && !args.length) return get();
+    if (isUndefined(value)) return _this;
+    for (const setter of $Node.setters) {
+        const result = setter(value, set);
+        if (!isUndefined(result)) return set(result), _this;
+    }
+    return set(value as any), _this;
+}
 
 interface chain {
     <T, V>(_this: T, args: null, get: null, value: V, set: (value: Exclude<V, undefined>) => any): T

0ace1ff459ae4f805e69b9aee129081c1eab64ee
defaultkavy <defaultkavy@gmail.com>
2025-10-20 17:10:59 +0800

    [css] added `@property` rule support.

---
+++ ext/css/src/ext/property.ts
@@ -0,0 +1,48 @@
+import { generateId } from "#lib/utils";
+import { $CSSProperty } from "#structure/$CSSProperty";
+import { $CSS } from "..";
+import { _instanceof, _Object_assign, _Object_entries, forEach, isString, isUndefined } from "amateras/lib/native";
+
+declare module 'amateras/core' {
+    export namespace $ {
+        export namespace css {
+            export function property<T extends $CSSPropertyMap>(resolver: T): { [key in keyof T]: $CSSProperty }
+            export function property(syntax: $CSSPropertySyntax, initialValue: string | number, inherits: boolean): $CSSProperty
+        }
+
+        export interface $CSSValueTypeExtendsMap {
+            property: $CSSProperty
+        }
+    }
+}
+
+export type $CSSPropertySyntax = 
+'<angle>' | '<color>' | '<custom-ident>' | 
+'<image>' | '<integer>' | '<length>' | 
+'<length-percentage>' | '<number>' | '<percentage>' | 
+'<resolution>' | '<string>' | '<time>' | 
+'<transform-function>' | '<transform-list>' | '<url>' |
+'auto' | '*' | string & {}
+
+export type $CSSPropertyMap = { [key: string]: [$CSSPropertySyntax, string | number, boolean] }
+
+_Object_assign($.css, {
+    property(resolver: $CSSPropertyMap | string, initialValue?: string | number, inherits?: boolean) {
+        if (isString(resolver)) {
+            if (isUndefined(initialValue) || isUndefined(inherits)) throw 'Register CSS Property Error';
+            const property = new $CSSProperty({ name: `--${generateId('lower')}`, syntax: resolver, initialValue: `${initialValue}`, inherits });
+            CSS.registerProperty(property)
+            return property;
+        } else {
+            const obj = {};
+            forEach(_Object_entries(resolver), ([key, [syntax, initialValue, inherits]]) => {
+                const property = new $CSSProperty({ name: `--${key}-${generateId('lower')}`, syntax, initialValue: `${initialValue}`, inherits });
+                _Object_assign(obj, { [key]: property });
+                CSS.registerProperty(property)
+            })
+            return obj;
+        }
+    }
+})
+
+$CSS.valueInstances.add($CSSProperty);
---
+++ ext/css/src/structure/$CSSProperty.ts
@@ -0,0 +1,19 @@
+import { _Object_assign } from "amateras/lib/native";
+
+export interface $CSSPropertyOptions {
+    name: string;
+    syntax: string;
+    initialValue: string;
+    inherits: boolean;
+}
+
+export interface $CSSProperty extends $CSSPropertyOptions {}
+export class $CSSProperty {
+    constructor(options: $CSSPropertyOptions) {
+        _Object_assign(this, options);
+    }
+
+    toString() {
+        return `var(${this.name})`
+    }
+}

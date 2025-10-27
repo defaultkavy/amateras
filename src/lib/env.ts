6182d58551e15af98c2abc7979c3cf1ffdc5b78b
defaultkavy <defaultkavy@gmail.com>
2025-10-27 19:18:05 +0800

    [core] support server enviroment

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,2 +1,10 @@
 // window and document
-export const _document = document;
+export let _window: Window;
+export let _document: Document;
+
+export const setGlobal = (w: Window) => {
+    _window = w;
+    _document = w.document;
+}
+
+if (typeof window !== 'undefined') setGlobal(window);

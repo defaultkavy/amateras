c8f41c8668bdf9c28625ab8531b1c27845885096
defaultkavy <defaultkavy@gmail.com>
2026-01-14 02:00:50 +0800

    [core] feat: GlobalState.dispose

--- packages/core/src/structure/GlobalState.ts
+++ packages/core/src/structure/GlobalState.ts
@@ -1,8 +1,14 @@
 import type { Proto } from "@amateras/core/structure/Proto";
+import { forEach } from "@amateras/utils";
 
 export class GlobalState {
+    static disposers = new Set<(global: GlobalState) => void>()
     constructor(proto: Proto) {
         let root = proto.root;
         if (root) return root.global;
+    }
+
+    dispose() {
+        forEach(GlobalState.disposers, disposer => disposer(this))
     }
 }

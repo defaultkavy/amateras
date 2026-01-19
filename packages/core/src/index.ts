f2a44fdb58cf4db4a54f72033d10c096bc1cb0f2
defaultkavy <defaultkavy@gmail.com>
2026-01-19 21:48:43 +0800

    [core] feat: `$.match` and `$.call` function

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -128,6 +128,36 @@ export namespace $ {
         proto.proto = cacheProtoParent;
     }
 
+    export const call = <T>(callback: () => T) => callback();
+
+    interface MatchCase<T, Rec extends T = never, Res = never, Def = never> {
+        case<C extends Exclude<T, Rec>, V>(condition: C, callback: () => V): MatchCase<T, Rec | C, Res | V, Def>
+        default<V>(callback: () => V): MatchCase<T, Rec, Res, V>
+    }
+
+    export const match = <T, M extends MatchCase<T>>(condition: T, callback: ($$: MatchCase<T>) => M): 
+        M extends MatchCase<T, infer Rec, infer Res, infer Def> 
+        ?   Exclude<T, Rec> extends never 
+            ?   Res 
+            :   Def extends never 
+                ?   Res | undefined 
+                :   Res | Def
+        :   never => {
+        var cases = new Map();
+        var symbol_default = Symbol('default');
+        var match = {
+            case: (condition: any, callback: () => any) => {
+                cases.set(condition, callback);
+                return match;
+            },
+            default: (callback: () => any) => {
+                cases.set(symbol_default, callback);
+                return match;
+            }
+        }
+        callback(match as any);
+        return cases.get(condition)?.() ?? cases.get(symbol_default)?.();
+    }
     export const stylesheet = onclient() ? new CSSStyleSheet() : _null;
     export const style = (css: string) => stylesheet?.insertRule(css);
     
@@ -144,3 +174,4 @@ export * from "#structure/NodeProto";
 export * from "#structure/Proto";
 export * from "#structure/ProxyProto";
 export * from "#structure/TextProto";
+

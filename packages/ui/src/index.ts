d76f2f368c83b25af50e35d2561636b3d0009925
defaultkavy <defaultkavy@gmail.com>
2026-01-20 12:09:20 +0800

    [ui/slideshow] feat: custom animation, ssr support

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,2 +1,3 @@
+export * from "#lib/slideshowAnimations";
 export * from "#structure/Slide";
 export * from "#structure/Slideshow";
--- packages/ui/src/structure/Slideshow.ts
+++ packages/ui/src/structure/Slideshow.ts
@@ -1,8 +1,6 @@
 import { ElementProto } from "@amateras/core";
-import { _Array_from, _null } from "@amateras/utils";
-import type { Slide } from "./Slide";
-
-$.style('slideshow{display:block;position:relative}')
+import { _null } from "@amateras/utils";
+import { Slide } from "./Slide";
 
 export interface SlideshowOptions {
     /**第一个播放的位置 */
@@ -11,32 +9,43 @@ export interface SlideshowOptions {
     interval?: number;
     /**是否自动播放 */
     autoplay?: boolean;
+    /**动画函数 */
+    animation?: SlideshowAnimationHandle;
 }
 
 export class Slideshow extends ElementProto {
+    declare __child__: Slide;
     slide: Slide | null = _null;
-    slides = new Set<Slide>();
-    declare protos: Set<Slide>;
     index: number;
     timer: NodeJS.Timeout | null = _null;
     interval: number;
     autoplay: boolean;
+    animation: SlideshowAnimationHandle | null;
     #passed = 0;
-    constructor({index, interval, autoplay, ...props}: $.Props<SlideshowOptions>, layout?: $.Layout<Slideshow>) {
+    constructor({index, interval, autoplay, animation, ...props}: $.Props<SlideshowOptions>, layout?: $.Layout<Slideshow>) {
         super('slideshow', props, layout);
         this.index = index ?? 0;
         this.interval = interval ?? 5;
         this.autoplay = autoplay ?? false;
+        this.animation = animation ?? _null;
         this.disposers.add(() => this.pause());
         this.ondom(() => {
             if (this.autoplay) this.play();
         })
     }
 
+    static {
+        $.style(Slideshow, 'slideshow{display:block;position:relative;overflow:clip}')
+    }
+
     override build(): this {
-        super.build().clear();
+        super.build();
         this.switch(this.index);
         return this;
+    }
+
+    override toString(): string {
+        return this.parseHTML({children: this.slide?.toString()});
     }
 
     override toDOM(children = true): HTMLElement[] {
@@ -60,14 +69,14 @@ export class Slideshow extends ElementProto {
     }
 
     next() {
-        let size = this.slides.size;
+        let size = this.children.length;
         let nextIndex = this.index + 1;
         if (nextIndex >= size) nextIndex = 0;
         this.switch(nextIndex);
     }
 
     prev() {
-        let size = this.slides.size;
+        let size = this.children.length;
         let prevIndex = this.index - 1;
         if (prevIndex <= 0) prevIndex = size - 1;
         this.switch(prevIndex);
@@ -75,36 +84,16 @@ export class Slideshow extends ElementProto {
 
     switch(index: number) {
         this.index = index;
-        let slide = this.getSlides().at(index);
-        if (!slide) throw 'Slideshow error: slide not found';
-        if (this.slide) this.animate(slide, this.slide);
+        let slide = this.children.at(index);
+        if (this.slide === slide) return;
+        if (!slide) return;
+        if (this.animation) this.animation(this, slide, this.slide);
         else {
             slide.parent = this;
             this.slide = slide;
+            this.node?.replaceChildren(...slide.toDOM());
         }
     }
-
-    getSlides() {
-        return _Array_from(this.slides);
-    }
-
-    animate(newSlide: Slide, oldSlide: Slide | null) {
-        this.slide = newSlide;
-        let newNodes = newSlide.toDOM();
-        this.node?.append(...newNodes);
-        newSlide.node?.animate({
-            translate: ['100% 0', '0 0']
-        }, {
-            duration: 500,
-            easing: 'ease'
-        })
-        const animation = oldSlide?.node?.animate({
-            translate: ['0 0', '-100% 0']
-        },{
-            duration: 500,
-            easing: 'ease'
-        })
-
-        if (animation) animation.onfinish = () => oldSlide?.node?.remove();
-    }
 }
+
+export type SlideshowAnimationHandle = (slideshow: Slideshow, newSlide: Slide, oldSlide: Slide | null) => void;

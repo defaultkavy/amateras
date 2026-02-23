caccb3d458d07ef567583cc5dae76472b51b1e21
defaultkavy <defaultkavy@gmail.com>
2026-02-23 16:41:15 +0800

    feat: [core] `GlobalState` new method `assign`, new property `promises` to cached promise objects

--- packages/core/src/structure/GlobalState.ts
+++ packages/core/src/structure/GlobalState.ts
@@ -1,9 +1,14 @@
-import { forEach } from "@amateras/utils";
+import { _Object_assign, forEach } from "@amateras/utils";
 
 export class GlobalState {
     static disposers = new Set<(global: GlobalState) => void>()
-
+    promises = new Set<Promise<any>>();
     dispose() {
+        this.promises.clear();
         forEach(GlobalState.disposers, disposer => disposer(this))
+    }
+
+    static assign(obj: object) {
+        _Object_assign(GlobalState.prototype, obj);
     }
 }

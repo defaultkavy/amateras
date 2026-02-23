f85e4d9a3f12e144a533e1491dd88afc46018cb9
defaultkavy <defaultkavy@gmail.com>
2026-02-23 16:30:44 +0800

    fix: [ui] export `TextBlock` and `Accordion`

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,3 +2,5 @@ export * from "#lib/slideshowAnimations";
 export * from "#structure/Slide";
 export * from "#structure/Slideshow";
 export * from "#structure/Radio";
+export * from "#structure/TextBlock";
+export * from "#structure/Accordion";

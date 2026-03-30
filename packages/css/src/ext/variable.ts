47b73ff488cef96259572e0a6ad7f3cc1807e4ba
defaultkavy <defaultkavy@gmail.com>
2026-03-31 01:41:09 +0800

    feat [css/variable]: add options to `$.css.variable` method for generate more customizable variable name

--- packages/css/src/ext/variable.ts
+++ packages/css/src/ext/variable.ts
@@ -7,25 +7,29 @@ declare global {
     export namespace $ {
         export namespace css {
             export function variable<V extends string>(value: V): $CSSVariable<V>;
-            export function variable<T extends $.CSSVariableType>(options: T): { [key in keyof T]: $CSSVariable<T[key]> }
+            export function variable<T extends $.CSSVariableType>(varMap: T, options?: $CSSVariableOptions): { [key in keyof T]: $CSSVariable<T[key]> }
         }
         
         export interface CSSValueMap {
-            variable: $CSSVariable
+            variable: $CSSVariable<string | number>
         }
 
         export type CSSVariableType<T = any> = { [key in keyof T]: $.CSSValue }
     }
 }
 
+export interface $CSSVariableOptions {
+    unique?: boolean,
+    prefix?: string
+}
 
 _Object_assign($.css, {
-    variable<T extends $.CSSVariableType | string>(options: T) {
-        let id = UID.generate('css-variable', {lettercase: 'lower'})
-        if (isObject(options)) {
+    variable<T extends $.CSSVariableType | string>(varMap: T, options?: $CSSVariableOptions) {
+        let id = options?.unique ? '_' + UID.generate('css-variable', {lettercase: 'lower'}) : '';
+        if (isObject(varMap)) {
             const variables = _Object_fromEntries(
-                map(_Object_entries(options), (([key, value]) => [
-                    key, new $CSSVariable(`--${camelCaseToDash(key)}_${id}`, `${value}`)
+                map(_Object_entries(varMap), (([key, value]) => [
+                    key, new $CSSVariable(`--${options?.prefix ?? ''}${camelCaseToDash(key)}${id}`, `${value}`)
                 ])
             ))
 
@@ -41,7 +45,7 @@ _Object_assign($.css, {
 
             return variables;
         } else {
-            const variable = new $CSSVariable(`--${id}`, options);
+            const variable = new $CSSVariable(`--${id}`, varMap);
             $.CSS({':root': {[variable.name]: variable.value}});
             return variable;
         }

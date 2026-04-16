f17324bdd53d9a7659cdf2d89cd05af61028bf1f
defaultkavy <defaultkavy@gmail.com>
2026-04-16 18:38:23 +0800

    feat [router]: now handle page switch behaviour is avalable with 'pageswitch' proto event

--- packages/router/src/structure/RouteSlot.ts
+++ packages/router/src/structure/RouteSlot.ts
@@ -2,26 +2,39 @@ import { onclient } from "@amateras/core";
 import { ProxyProto } from "@amateras/core";
 import { _null } from "@amateras/utils";
 import { Page } from "./Page";
+import { Router } from "./Router";
 
 export class RouteSlot extends ProxyProto {
     page: Page | null = _null;
+    prevPage: Page | null = _null;
     constructor() {
         super();
     }
 
     switch(page: Page) {
         if (this.page === page) return;
+        this.prevPage = this.page;
         this.clear();
         this.layout = () => $(page);
         this.append(page);
-        if (this.page !== page) this.page?.removeNode();
         this.page = page;
         if (!page.builded) page.build();
         page.updateTitle();
-        
+
+        if (!this.dispatch('pageswitch', [this, Router.direction], {bubbles: true})) this.render();
+    }
+
+    render() {
         if (onclient()) {
+            this.prevPage?.removeNode();
             let nodes = this.toDOM();
             this.node?.replaceWith(...nodes);
         }
+    }
+
+    override dispose(): void {
+        super.dispose();
+        this.page = _null;
+        this.prevPage = _null;
     }
 }

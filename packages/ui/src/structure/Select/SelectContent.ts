92c4b15626c58460012ce88f1dbb93920cbb6bba
defaultkavy <defaultkavy@gmail.com>
2026-04-16 17:48:46 +0800

    style [ui]: import css of `SelectContent` component
    - Limited max height and overflow auto.
    - Styled scrollbar.
    - Disable user-select behavior.

--- packages/ui/src/structure/Select/SelectContent.ts
+++ packages/ui/src/structure/Select/SelectContent.ts
@@ -15,13 +15,18 @@ export class SelectContent extends ElementProto {
             position: 'absolute',
             top: '0',
             left: '0',
+            maxHeight: '50dvh',
+            overflowY: 'auto',
             display: 'block',
             boxSizing: 'border-box',
             border: '1px solid var(--input)',
             background: 'oklch(from var(--bg) l c h)',
             padding: 'calc(var(--spacing) * 2) calc(var(--spacing) * 1.25)',
             borderRadius: 'var(--radius)',
-            userSelect: 'none'
+            userSelect: 'none',
+
+            scrollbarWidth: 'thin',
+            scrollbarColor: 'var(--input) transparent',
         }))
     }
 

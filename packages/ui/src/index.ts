ace2ff6930b8999967617520d4d091113c8b1527
defaultkavy <defaultkavy@gmail.com>
2026-05-06 13:16:58 +0800

    feat [ui/text-block]: remove `TextBlock` component

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,7 +2,6 @@ export * from "#lib/slideshowAnimations";
 export * from "#structure/Slide";
 export * from "#structure/Slideshow";
 export * from "#structure/Radio";
-export * from "#structure/TextBlock";
 export * from "#structure/Accordion";
 export * from "#structure/Waterfall";
 export * from "#structure/WaterfallItem";

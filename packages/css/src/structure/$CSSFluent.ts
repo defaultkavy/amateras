3823114503b51c849df52a1d10e33c2b4aee70ca
defaultkavy <defaultkavy@gmail.com>
2026-05-26 13:04:22 +0800

    chore [css]: improve css fluent configure process

--- packages/css/src/structure/$CSSFluent.ts
+++ packages/css/src/structure/$CSSFluent.ts
@@ -5,7 +5,6 @@ declare const map: unique symbol;
 export class $CSSFluent<M extends Record<string, CSSFluentGroupMap> = {}> {
     declare [map]: M;
     initRule: $.CSSDeclarationMap;
-    groupMap = new Map<string, CSSFluentGroup>();
     optionMap = new Map<string, $.CSSDeclarationMap>();
     target = {};
     constructor(initRule?: $.CSSDeclarationMap) {
@@ -17,39 +16,26 @@ export class $CSSFluent<M extends Record<string, CSSFluentGroupMap> = {}> {
         return this as $CSSFluent<Prettify<M & Record<N, {[key in N]: $.CSSDeclarationMap}>>>;
     }
 
-    group<P extends keyof $.CSSDeclarationMap | string & {}, H extends (group: CSSFluentGroup<P>) => CSSFluentGroup>(propertyName: P, handle: H) {
-        const group = handle(new CSSFluentGroup(propertyName));
-        Utils.forEach(Utils.entries(group.options), ([name]) => this.groupMap.set(name, group));
-        return this as $CSSFluent<Prettify<M & Record<P, ReturnType<H>[typeof map]>>>
+    group<P extends keyof $.CSSDeclarationMap | string & {}, O extends CSSFluentGroupMap<P extends keyof $.CSSDeclarationMap ? Required<$.CSSDeclarationMap>[P] : $.CSSValue>>(prop: P, options: O) {
+        Utils.forEach(Utils.entries(options), ([name, value]) => this.optionMap.set(name, {[prop]: value}));
+        return this as unknown as $CSSFluent<Prettify<M & Record<P, O>>>
     }
 
     proxy() {
         this.reset();
         const proxy = new Proxy(this.target, {
-            get: (target, propName) => {
+            get: (_, propName) => {
+                const target = this.target;
                 if (!Utils.isString(propName)) return;
                 if (propName === '$') {
                     const cssMap = {...target};
                     this.reset();
                     return cssMap
                 }
-                const group = this.groupMap.get(propName);
                 const option = this.optionMap.get(propName);
-                if (option) {
-                    Utils.assign(target, option)
-                }
-                if (group) {
-                    const value = group.options[propName];
-                    Utils.assign(target, { [group.propertyName]: value })
-                }
+                if (option) Utils.assign(target, option)
                 return proxy;
-            },
-            getOwnPropertyDescriptor(target, p) {
-                return Reflect.getOwnPropertyDescriptor(target, p);
-            },
-            ownKeys(target) {
-                return Reflect.ownKeys(target).map(prop => `_${prop as string}`)
-            },
+            }
         }) as CSSFluentProxy<M>
         return proxy
     }
@@ -59,20 +45,20 @@ export class $CSSFluent<M extends Record<string, CSSFluentGroupMap> = {}> {
     }
 }
 
-type CSSFluentGroupMap = Record<string, $.CSSValue>;
+type CSSFluentGroupMap<V extends $.CSSValue = $.CSSValue> = Record<string, V>;
 
-export class CSSFluentGroup<P extends string = any, M extends CSSFluentGroupMap = {}> {
-    declare [map]: M;
-    propertyName: P;
-    options: CSSFluentGroupMap = {};
-    constructor(propertyName: P) {
-        this.propertyName = propertyName;
-    }
-    option<N extends string, V extends P extends keyof $.CSSDeclarationMap ? Required<$.CSSDeclarationMap>[P] : $.CSSValue>(name: N, value: V) {
-        this.options[name] = value;
-        return this as unknown as CSSFluentGroup<P, Prettify<M & Record<N, V>>>;
-    }
-}
+// export class CSSFluentGroup<P extends string = any, M extends CSSFluentGroupMap = {}> {
+//     declare [map]: M;
+//     propertyName: P;
+//     options: CSSFluentGroupMap = {};
+//     constructor(propertyName: P) {
+//         this.propertyName = propertyName;
+//     }
+//     option<N extends string, V extends P extends keyof $.CSSDeclarationMap ? Required<$.CSSDeclarationMap>[P] : $.CSSValue>(name: N, value: V) {
+//         this.options[name] = value;
+//         return this as unknown as CSSFluentGroup<P, Prettify<M & Record<N, V>>>;
+//     }
+// }
 
 export type CSSFluentProxy<M extends Record<string, CSSFluentGroupMap>> = {
     [key in KeyofUnion<M[KeyofUnion<M>]>]: CSSFluentProxy<Omit<M, GetMapKeyWithKey<key, M>>>

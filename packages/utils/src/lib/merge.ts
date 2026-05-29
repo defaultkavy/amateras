edcc0b3d4a53963389e308e2e6c89a84aa2fca85
defaultkavy <defaultkavy@gmail.com>
2026-05-30 00:17:17 +0800

    chore [utils]: complete `merge` utility function type

--- packages/utils/src/lib/merge.ts
+++ packages/utils/src/lib/merge.ts
@@ -1,8 +1,12 @@
 import { forEach } from "./forEach";
 import { assign } from "./object";
 
-export const merge = (objects: Object[]) => {
+type UnionMerge<T> = {
+    [key in (T extends any ? keyof T : never)]: T extends any ? (key extends keyof T ? T[key] : never) : never
+}
+
+export const merge = <T extends any[]>(...objects: T): Prettify<UnionMerge<T[number]>> => {
     let merged = {};
     forEach(objects, obj => assign(merged, obj));
-    return merged;
+    return merged as any;
 }

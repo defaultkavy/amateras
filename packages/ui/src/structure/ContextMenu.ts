2bcc2a88ed8b25ae64e1a8233e1b6997626ed8bd
defaultkavy <defaultkavy@gmail.com>
2026-05-31 14:17:37 +0800

    style [ui/context-menu]: change `ContextMenu` background color to secondary

--- packages/ui/src/structure/ContextMenu.ts
+++ packages/ui/src/structure/ContextMenu.ts
@@ -17,7 +17,7 @@ export class ContextMenu extends ElementProto {
             overflowY: 'auto',
             boxSizing: 'border-box',
             border: '1px solid var(--input)',
-            background: 'oklch(from var(--bg) l c h)',
+            background: 'oklch(from var(--secondary-bg) l c h)',
             padding: 'calc(var(--spacing) * 2) calc(var(--spacing) * 1.25)',
             borderRadius: 'var(--radius)',
             userSelect: 'none',

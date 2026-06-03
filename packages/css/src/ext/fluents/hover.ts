9b225317bd37c74145d143ec6f159e37d048f7cc
defaultkavy <defaultkavy@gmail.com>
2026-06-03 21:05:54 +0800

    chore [css]: improve `hover` function type

--- packages/css/src/ext/fluents/hover.ts
+++ packages/css/src/ext/fluents/hover.ts
@@ -1,6 +1,6 @@
 import '#ext/fluent';
 import { Utils } from "@amateras/utils";
 
-export const hover = (map: OrArray<$.CSSDeclarationMap>) => ({
+export const hover = (map: OrArray<$.CSSMap>) => ({
     'html:not([touch]) &:hover': Utils.merge(...Utils.toArray(map))
 })

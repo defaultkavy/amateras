9db30b2d4ba5371a08eaf97d9771035bea56312f
defaultkavy <defaultkavy@gmail.com>
2026-06-05 06:14:45 +0800

    feat [router]: `NavLink` support active when location matched exactly

--- packages/router/src/structure/NavLink.ts
+++ packages/router/src/structure/NavLink.ts
@@ -1,9 +1,13 @@
 import { Link } from "#structure/Link";
 import { Utils } from '@amateras/utils';
 
+export interface NavLinkProps {
+    exact?: boolean;
+}
+
 export class NavLink extends Link {
-    constructor(attr: $.Props | null, layout?: $.Layout<NavLink>) {
-        super(attr, layout as any);
+    constructor(props: $.Props<NavLinkProps> | null, layout?: $.Layout<NavLink>) {
+        super(props, layout as any);
         this.global.router.navlinks.add(this);
     }
 
@@ -15,8 +19,10 @@ export class NavLink extends Link {
     checkActive() {
         let href = this.attr('href');
         if (!href) return;
-        for (let path of this.global.router.matchPaths) {
-            if (Utils.toURL(path).href === Utils.toURL(href).href) return this.attr('active', ''); 
+        const paths = this.global.router.matchPaths;
+        for (let path of paths) {
+            if (this.attr('exact') === 'true' && paths.indexOf(path) !== paths.length - 1) continue;
+            if (Utils.toURL(path).href.replace(/\/$/, '') === Utils.toURL(href).href.replace(/\/$/, '')) return this.attr('active', '');
         }
         this.attr('active', Utils.Null);
     }

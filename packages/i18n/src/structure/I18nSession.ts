816c42a474050e6fd562243708972a40ace8974f
defaultkavy <defaultkavy@gmail.com>
2026-06-10 15:02:04 +0800

    feat [i18n]: new signal value `I18nSession.locale$`

--- packages/i18n/src/structure/I18nSession.ts
+++ packages/i18n/src/structure/I18nSession.ts
@@ -3,17 +3,20 @@ import type { I18nTranslationResult } from "../types";
 import type { I18n } from "./I18n";
 import type { I18nTranslation, I18nTranslationOptions } from "./I18nTranslation";
 import { GlobalState, onclient } from "@amateras/core";
+import type { Signal } from '@amateras/signal';
 
 export class I18nSession {
     translations = new Set<I18nTranslation>()
     i18n: I18n;
     #locale: string;
+    locale$: Signal<string>;
     global: GlobalState;
     listeners = new Set<I18nSessionListener>();
     constructor(i18n: I18n, global: GlobalState) {
         this.i18n = i18n;
         this.global = global;
         this.#locale = i18n.locale();
+        this.locale$ = $.signal(this.#locale)
         i18n.sessions.add(this);
     }
     
@@ -42,6 +45,7 @@ export class I18nSession {
         }
         if (locale && locale !== this.#locale) {
             this.#locale = locale;
+            this.locale$.set(locale);
             return new Promise<void>(async (resolve) => {
                 await Promise.all(Utils.map(this.translations, translation => translation.update()));
                 resolve();
@@ -57,6 +61,8 @@ export class I18nSession {
         this.i18n.sessions.delete(this);
         this.i18n = Utils.Null as any;
         this.translations = Utils.Null as any;
+        this.locale$.dispose();
+        this.locale$ = Utils.Null as any;
     }
 }
 

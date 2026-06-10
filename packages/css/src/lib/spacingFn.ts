af641b9325f1adcff2830b3343d68b11efe923b2
defaultkavy <defaultkavy@gmail.com>
2026-06-10 15:01:42 +0800

    chore [css/fluent]: `spacingFn` support `$CSSVariable` as input

--- packages/css/src/lib/spacingFn.ts
+++ packages/css/src/lib/spacingFn.ts
@@ -1,3 +1,3 @@
 import { Utils } from "@amateras/utils";
 
-export const spacingFn = (val: $.CSSValue) => Utils.isString(val) ? val : `calc(var(--spacing) * ${val})`;
+export const spacingFn = (val: $.CSSValue) => Utils.isNumber(val) ? `calc(var(--spacing) * ${val})` : val;

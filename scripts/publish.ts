bf1c830e75f0b091d007388abfe07bff63e7c061
defaultkavy <defaultkavy@gmail.com>
2026-08-19 14:47:17 +0800

    chore: scripts/publish.ts

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -3,13 +3,41 @@ import { readdir } from "node:fs/promises";
 const args = Bun.argv.slice(2);
 
 const tag = prompt('Publish Tag (null): ');
-const target = prompt('Target: ')
 
-const packagesPath = `${process.cwd()}/packages`;
-const packageFolders = await readdir(packagesPath);
+const packagesDir = `${process.cwd()}/packages`;
+const packageFolders = await readdir(packagesDir);
 
-for (const folderName of packageFolders) {
+const publishPackages: {name: string, dir: string, version: string, registryVersion: string}[] = [];
+
+console.log('Comparing packages version from registry...');
+await Promise.all(packageFolders.map(async folderName => {
+    const packagePath = `${packagesDir}/${folderName}`;
     const packageName = folderName === 'amateras' ? folderName : `@amateras/${folderName}`;
-    const publishArgs = tag?.trim().length ? `--tag ${tag.trim()}` : '';
-    Bun.$`(cd ${packagesPath}/${folderName} && bun publish ${publishArgs})`;
+    const version = await Bun.$`(cd ${packagePath} && bun pm pkg get version)`.quiet().then(res => res.json() as string);
+    const registryVersion = await Bun.$`bun info ${packageName} version`.quiet().then(res => res.text().trim()).catch(() => 'none');
+    if (version === registryVersion) return;
+    publishPackages.push({
+        name: packageName,
+        dir: packagePath,
+        version: version,
+        registryVersion
+    });
+}))
+
+console.log(`Publish Packages:\n${publishPackages.map(pkg => {
+    const length = `+ ${pkg.name}`.length;
+    return `+ ${pkg.name.slice(0, 40)}${' '.repeat(40 - length)}${pkg.registryVersion} -> ${pkg.version}`
+}).join('\n')}`)
+
+const confirm = prompt('Publish all packages? (y/N)');
+
+if (!confirm) process.exit();
+else if (confirm.toLowerCase() === 'n') process.exit();
+else if (confirm.toLowerCase() === 'y') {
+    await Bun.$`bun update`.quiet();
+    for (const pkg of publishPackages) {
+        const publishArgs = tag?.trim().length ? [`--tag ${tag.trim()}`] : [];
+        await Bun.$`(cd ${pkg.dir} && bun publish --access public)`;
+    }
+    console.log('Done')
 }
